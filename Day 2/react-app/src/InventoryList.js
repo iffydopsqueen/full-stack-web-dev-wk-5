c363def2183edd4fd124acd4f0971c5ecfccc11a
Iffy Eleweke <omaleweks@gmail.com>
2024-04-02 11:33:18 -0400

    Added the inventory edit component.

--- Day 2/react-app/src/InventoryList.js
+++ Day 2/react-app/src/InventoryList.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Button, ButtonGroup, Container, Table } from "reactstrap";
 import AppNavbar from "./Navbar";
 import { Link } from "react-router-dom";
-import { Tab } from "bootstrap";
 
 class InventoryList extends Component {
     constructor(props) {
